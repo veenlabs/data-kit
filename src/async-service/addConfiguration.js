d7483126d1d9cf6786ac4604af1cfa7cc83c4d54
Praveen Prasad <veen.edu@gmail.com>
2022-05-13 22:03:29 +0530

    Minor fix

--- src/async-service/addConfiguration.js
+++ src/async-service/addConfiguration.js
@@ -1,8 +1,8 @@
-import { setCache, NAMESPACES } from '../helpers/cache'
-import { ASYNC_SERVICE_DEFAULT_PROVIDER_NAME } from '../helpers/const'
+import { setCache } from '../helpers/cache'
+import { ASYNC_SERVICE_DEFAULT_PROVIDER_NAME, CACHE_NAMESPACES } from '../helpers/const'
 
 function addConfiguration(config) {
-  setCache(NAMESPACES.ASYNC_SERVICE_CONFIG, config.name || ASYNC_SERVICE_DEFAULT_PROVIDER_NAME, config)
+  setCache(CACHE_NAMESPACES.ASYNC_SERVICE_CONFIG, config.name || ASYNC_SERVICE_DEFAULT_PROVIDER_NAME, config)
 }
 
 export default addConfiguration
--- src/async-service/addOperations.js
+++ src/async-service/addOperations.js
@@ -1,12 +1,12 @@
 import { get } from './lodash'
-import { ASYNC_SERVICE_DEFAULT_PROVIDER_NAME } from '../helpers/const'
-import { setCache, getCache, NAMESPACES } from '../helpers/cache'
+import { ASYNC_SERVICE_DEFAULT_PROVIDER_NAME, CACHE_NAMESPACES } from '../helpers/const'
+import { setCache, getCache } from '../helpers/cache'
 
 function addOperations(options) {
   const { _config, ...operations } = options
   const providerName = get(_config, 'name', ASYNC_SERVICE_DEFAULT_PROVIDER_NAME)
-  const optionsToSave = { ...getCache(NAMESPACES.ASYNC_SERVICE_OPERATIONS_OPTIONS, providerName, {}), providerName }
-  setCache(NAMESPACES.ASYNC_SERVICE_OPERATIONS_OPTIONS, providerName, optionsToSave)
+  const optionsToSave = { ...getCache(CACHE_NAMESPACES.ASYNC_SERVICE_OPERATIONS_OPTIONS, providerName, {}), providerName }
+  setCache(CACHE_NAMESPACES.ASYNC_SERVICE_OPERATIONS_OPTIONS, providerName, optionsToSave)
 }
 
 export { addOperations }
