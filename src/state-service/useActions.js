9c4cb1486768d318b380fcd340cd20a2be299328
Praveen Prasad <veen.edu@gmail.com>
2022-05-17 20:53:48 +0530

    Update useActions

--- src/state-service/useActions.js
+++ src/state-service/useActions.js
@@ -2,11 +2,32 @@ import { useDispatch } from 'react-redux'
 import { getCache } from '../helpers/cache'
 import { CACHE_NAMESPACES } from '../helpers/const'
 import actionsProxyHandler from './actionCreatorProxyHandler'
+import { getActionTypeFromPath, isObjectReactSyntheticEvent } from './utils'
 
-function useActions({ name }) {
-  const slice = getCache(CACHE_NAMESPACES.STATE_SERVICE_RAW_SLICES, name)
+function useActions(params) {
+  // Params can be slice or a pattern
+  //params => slice
+  //params => pathString => user:authentication, products:get:request
+
   const dispatch = useDispatch()
-  return new Proxy({ slice, dispatch }, actionsProxyHandler)
+  const stringPattern = typeof params === 'string' ? params : params.name
+  const [sliceName, actionName, step] = stringPattern.split(':')
+  const slice = getCache(CACHE_NAMESPACES.STATE_SERVICE_RAW_SLICES, sliceName)
+  if (step) {
+    return (data) => {
+      dispatch({
+        type: getActionTypeFromPath([sliceName, actionName, step]),
+        payload: isObjectReactSyntheticEvent(data) ? null : data,
+      })
+    }
+  } else if (actionName) {
+    return (data) => {
+      const p = new Proxy({ slice, dispatch }, actionsProxyHandler)
+      p[actionName](data)
+    }
+  } else {
+    return new Proxy({ slice, dispatch }, actionsProxyHandler)
+  }
 }
 
 export default useActions
