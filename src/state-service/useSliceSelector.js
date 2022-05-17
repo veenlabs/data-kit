3ddafe43300b62a500206a8c6aafd4b56f886697
Praveen Prasad <veen.edu@gmail.com>
2022-05-17 15:23:52 +0530

    Minor refactoring

--- src/state-service/useSliceSelector.js
+++ src/state-service/useSliceSelector.js
@@ -3,21 +3,31 @@ import { getCache } from '../helpers/cache'
 import { CACHE_NAMESPACES } from '../helpers/const'
 import { get, identity } from '../helpers/lodash'
 
+function renSelector(sliceName, selectorName, state) {
+  const slice = getCache(CACHE_NAMESPACES.STATE_SERVICE_RAW_SLICES, sliceName)
+  const path = ['selectors', selectorName]
+  const currentSelector = get(slice, path, null)
+
+  try {
+    if (currentSelector) {
+      return currentSelector(state[slice.name], state)
+    } else {
+      console.warn("Invalid selector: this selector doesn't exist in slice", { slice: get(slice, 'name'), selectorName })
+      return `Invalid selector ${slice.name}.${selectorName}`
+    }
+  } catch (error) {
+    console.warn('Error thrown from selector', path)
+    console.warn(error)
+  }
+}
+
 function useSliceSelector(sliceRef) {
   const selector = (s) => {
     return new Proxy(
       {},
       {
-        get(target, prop) {
-          const slice = getCache(CACHE_NAMESPACES.STATE_SERVICE_RAW_SLICES, sliceRef.name)
-          const path = ['selectors', prop]
-          const currentSelector = get(slice, path, null)
-          try {
-            return currentSelector(s[slice.name], s)
-          } catch (error) {
-            console.warn('Error thrown from selector', path)
-            console.warn(error)
-          }
+        get(target, selectorName) {
+          return renSelector(sliceRef.name, selectorName, s)
         },
       },
     )
@@ -29,17 +39,7 @@ function useSliceSelector(sliceRef) {
 function useSliceSelector2(selectorPath = '') {
   // selectorPath = sliceName:selector -> user:getFullName
   const [sliceName, selectorName] = selectorPath.split(':')
-  const selector = (s) => {
-    const slice = getCache(CACHE_NAMESPACES.STATE_SERVICE_RAW_SLICES, sliceName)
-    const path = ['selectors', selectorName]
-    const currentSelector = get(slice, path, null)
-    try {
-      return currentSelector(s[slice.name], s)
-    } catch (error) {
-      console.warn('Error thrown from selector', path)
-      console.warn(error)
-    }
-  }
+  const selector = (s) => renSelector(sliceName, selectorName, s)
   const result = useSelector(selector)
   return result
 }
