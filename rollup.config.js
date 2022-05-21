e9046ab3a6fe1611e4907592301afb5ca219c921
Praveen Prasad <veen.edu@gmail.com>
2022-05-21 21:58:39 +0530

    Prepare for release

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,21 @@ import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
-// import postcss from 'rollup-plugin-postcss';
+import filesize from 'rollup-plugin-filesize';
 
 export default [
   {
     input: './src/index.js',
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react'],
+      }),
+      external(),
+      resolve(),
+      filesize(),
+      process.env.NODE_ENV === 'production' && terser(),
+    ],
     output: [
       {
         file: 'dist/index.js',
@@ -14,24 +24,29 @@ export default [
         sourcemap: true,
       },
       {
-        file: 'dist/index.es.js',
+        file: 'dist/veen-data-kit.es.js',
         format: 'es',
-        exports: 'named',
         sourcemap: true,
+        exports: 'named',
+      },{
+        file: 'dist/veen-data-kit.umd.js',
+        format: 'umd',
+        name: 'VeenDataKit',
+        globals: {
+          react: 'React',
+          axios: 'Axios',
+          'react-redux': 'ReactRedux',
+          'redux-saga': 'ReduxSaga',
+          'redux-saga/effects': 'ReduxSaga/Effects',
+          'redux-logger': 'ReactLogger',
+          'redux': 'Redux',
+          'lodash/get': 'Lodash/Get',
+          'lodash/set': 'Lodash/Set',
+          'lodash/identity': 'Lodash/Identity',
+          'lodash/identity': 'Lodash/Identity',
+          'immer': 'Immer',
+        },
       }
-    ],
-    plugins: [
-    //   postcss({
-    //     plugins: [],
-    //     minimize: true,
-    //   }),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-      }),
-      external(),
-      resolve(),
-      terser(),
     ]
   }
 ];
